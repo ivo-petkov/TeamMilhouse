294c1560c49202d2309b6f5ab90a56eca71a5d8c
Nikola Kostov <nikola_kostov92@mail.bg>
2013-08-14 20:15:23 +0300

    Added Dropbox file upload

---
+++ WebChat/WebChat.DropboxUploader/DropboxFileUploader.cs
@@ -0,0 +1,84 @@
+﻿using Spring.IO;
+using Spring.Social.Dropbox.Api;
+using Spring.Social.Dropbox.Connect;
+using Spring.Social.OAuth1;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebChat.DropboxUploader
+{
+    public class DropboxFileUploader
+    {
+        private const string DropboxAppKey = "4a5uo0dehhx80mr";
+        private const string DropboxAppSecret = "us7kwtooy1lbm2m";
+
+        private static string OAuthTokenFileName = System.Web.HttpContext.Current.Server.MapPath("~/OAuthTokenFileName.txt");
+
+        public static string FileUpload(string path)
+        {
+            DropboxServiceProvider dropboxServiceProvider =
+                new DropboxServiceProvider(DropboxAppKey, DropboxAppSecret, AccessLevel.AppFolder);
+
+            // Authenticate the application (if not authenticated) and load the OAuth token
+            if (!File.Exists(OAuthTokenFileName))
+            {
+                AuthorizeAppOAuth(dropboxServiceProvider);
+            }
+            OAuthToken oauthAccessToken = LoadOAuthToken();
+
+            // Login in Dropbox
+            IDropbox dropbox = dropboxServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+
+            // Display user name (from his profile)
+            DropboxProfile profile = dropbox.GetUserProfileAsync().Result;
+
+            // Create new folder
+            string newFolderName = "Avatars";
+            //Entry createFolderEntry = dropbox.CreateFolderAsync(newFolderName).Result;
+            //Console.WriteLine("Created folder: {0}", createFolderEntry.Path);
+
+            // Upload a file
+            Entry uploadFileEntry = dropbox.UploadFileAsync(
+                new FileResource(System.Web.HttpContext.Current.Server.MapPath("~/" + path)),
+                "/" + newFolderName + "/" + path).Result;
+
+            // Share a file
+            DropboxLink sharedUrl = dropbox.GetShareableLinkAsync(uploadFileEntry.Path).Result;
+            //Process.Start(sharedUrl.Url);
+            return sharedUrl.Url;
+        }
+
+        private static OAuthToken LoadOAuthToken()
+        {
+            string[] lines = File.ReadAllLines(OAuthTokenFileName);
+            OAuthToken oauthAccessToken = new OAuthToken(lines[0], lines[1]);
+            return oauthAccessToken;
+        }
+
+        private static void AuthorizeAppOAuth(DropboxServiceProvider dropboxServiceProvider)
+        {
+            // Authorization without callback url
+            OAuthToken oauthToken = dropboxServiceProvider.OAuthOperations.FetchRequestTokenAsync(null, null).Result;
+
+            OAuth1Parameters parameters = new OAuth1Parameters();
+            string authenticateUrl = dropboxServiceProvider.OAuthOperations.BuildAuthorizeUrl(
+                oauthToken.Value, parameters);
+            Process.Start(authenticateUrl);
+            System.Threading.Thread.Sleep(5000);
+
+            Console.Write("Getting access token...");
+            AuthorizedRequestToken requestToken = new AuthorizedRequestToken(oauthToken, null);
+            OAuthToken oauthAccessToken =
+                dropboxServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
+            Console.WriteLine("Done.");
+
+            string[] oauthData = new string[] { oauthAccessToken.Value, oauthAccessToken.Secret };
+            File.WriteAllLines(OAuthTokenFileName, oauthData);
+        }
+    }
+}
---
+++ WebChat/WebChat.Services/Controllers/FileController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebChat.Model;
+using WebChat.Data;
+using System.IO;
+using WebChat.DropboxUploader;
+
+namespace WebChat.Services.Controllers
+{
+    public class FileController : ApiController
+    {
+        //    // GET api/values
+        //    public IEnumerable<string> Get()
+        //    {
+        //        return new string[] { "value1", "value2" };
+        //    }
+
+        //    // GET api/values/5
+        public HttpResponseMessage Get(string id)
+        {
+            HttpResponseMessage result = null;
+            var localFilePath = HttpContext.Current.Server.MapPath("~/" + id);
+
+            // check if parameter is valid
+            if (String.IsNullOrEmpty(id))
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            // check if file exists on the server
+            else if (!System.IO.File.Exists(localFilePath))
+            {
+                result = Request.CreateResponse(HttpStatusCode.Gone);
+            }
+            else
+            {// serve the file to the client
+                result = Request.CreateResponse(HttpStatusCode.OK);
+                result.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
+                result.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                result.Content.Headers.ContentDisposition.FileName = id;
+            }
+
+            return result;
+        }
+
+        public HttpResponseMessage Post()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+
+            // Check if files are available
+            if (httpRequest.Files.Count > 0)
+            {
+                var files = new List<string>();
+
+                // interate the files and save on the server
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+
+
+                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+
+                    var dropBoxUrl = DropboxFileUploader.FileUpload(postedFile.FileName);
+
+                    System.IO.File.Delete(HttpContext.Current.Server.MapPath("~/" + postedFile.FileName));
+
+                    files.Add(dropBoxUrl);
+                }
+
+                // return result
+                result = Request.CreateResponse(HttpStatusCode.Created, files);
+            }
+            else
+            {
+                // return BadRequest (no file(s) available)
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            return result;
+        }
+    }
+}
