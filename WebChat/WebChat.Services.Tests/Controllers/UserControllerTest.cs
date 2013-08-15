c7c17c4b0066629b2af0380bbdbfc4f80a5f9a29
Nikola Kostov <nikola_kostov92@mail.bg>
2013-08-15 15:20:21 +0300

    No tests

--- WebChat/WebChat.Services.Tests/Controllers/UserControllerTest.cs
+++ WebChat/WebChat.Services.Tests/Controllers/UserControllerTest.cs
@@ -1,88 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebChat.Services;
-using WebChat.Services.Controllers;
-using System.Transactions;
-using System.Net;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Text;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using WebChat.Services;
+//using WebChat.Services.Controllers;
+//using System.Transactions;
+//using System.Net;
 
-namespace WebChat.Services.Tests.Controllers
-{
-    [TestClass]
-    public class UserControllerTest
-    {
-        //[TestMethod]
-        //public void Get()
-        //{
-        //    // Arrange
-        //    ValuesController controller = new ValuesController();
+//namespace WebChat.Services.Tests.Controllers
+//{
+//    [TestClass]
+//    public class UserControllerTest
+//    {
+//        //[TestMethod]
+//        //public void Get()
+//        //{
+//        //    // Arrange
+//        //    ValuesController controller = new ValuesController();
 
-        //    // Act
-        //    IEnumerable<string> result = controller.Get();
+//        //    // Act
+//        //    IEnumerable<string> result = controller.Get();
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //    Assert.AreEqual(2, result.Count());
-        //    Assert.AreEqual("value1", result.ElementAt(0));
-        //    Assert.AreEqual("value2", result.ElementAt(1));
-        //}
+//        //    // Assert
+//        //    Assert.IsNotNull(result);
+//        //    Assert.AreEqual(2, result.Count());
+//        //    Assert.AreEqual("value1", result.ElementAt(0));
+//        //    Assert.AreEqual("value2", result.ElementAt(1));
+//        //}
 
-        [TestMethod]
-        public void GetById()
-        {
-            // Arrange
-            UserController controller = new UserController();
+//        [TestMethod]
+//        public void GetById()
+//        {
+//            // Arrange
+//            UserController controller = new UserController();
 
-            // Act
-            var user = new UserLoginModel
-            {
-                Username = "pesho111",
-                AuthCode = "152c75d70810ffba3ad4bf931089925c727660f1" // talantliv
-            };
+//            // Act
+//            var user = new UserLoginModel
+//            {
+//                Username = "pesho111",
+//                AuthCode = "152c75d70810ffba3ad4bf931089925c727660f1" // talantliv
+//            };
 
-            var result = controller.LoginUser(user);
+//            var result = controller.LoginUser(user);
 
-            // Assert
-            var expected = new HttpRequestMessage();
-            Assert.AreEqual(expected.CreateResponse(HttpStatusCode.OK), result);
-        }
+//            // Assert
+//            var expected = new HttpRequestMessage();
+//            Assert.AreEqual(expected.CreateResponse(HttpStatusCode.OK), result);
+//        }
 
-        //[TestMethod]
-        //public void Post()
-        //{
-        //    // Arrange
-        //    UserController controller = new UserController();
+//        //[TestMethod]
+//        //public void Post()
+//        //{
+//        //    // Arrange
+//        //    UserController controller = new UserController();
 
-        //    // Act
-        //    var user = new UserLoginModel
-        //    {
-        //        Username = "talantliv",
-        //        AuthCode = "d9fe89ffc19310e99f6cddef64bab7edd4ccfc50" // talantliv
-        //    };
+//        //    // Act
+//        //    var user = new UserLoginModel
+//        //    {
+//        //        Username = "talantliv",
+//        //        AuthCode = "d9fe89ffc19310e99f6cddef64bab7edd4ccfc50" // talantliv
+//        //    };
 
-        //    using (var tran = new TransactionScope())
-        //    {
-        //        var response = controller.RegisterUser(user);
-        //        var expected = new HttpRequestMessage();
-        //        Assert.AreEqual(expected.CreateResponse(HttpStatusCode.OK), response.StatusCode);
-        //    }
+//        //    using (var tran = new TransactionScope())
+//        //    {
+//        //        var response = controller.RegisterUser(user);
+//        //        var expected = new HttpRequestMessage();
+//        //        Assert.AreEqual(expected.CreateResponse(HttpStatusCode.OK), response.StatusCode);
+//        //    }
 
-        //    // Assert
-        //}
+//        //    // Assert
+//        //}
 
-        //[TestMethod]
-        //public void Put()
-        //{
-        //    // Arrange
-        //    ValuesController controller = new ValuesController();
+//        //[TestMethod]
+//        //public void Put()
+//        //{
+//        //    // Arrange
+//        //    ValuesController controller = new ValuesController();
 
-        //    // Act
-        //    controller.Put(5, "value");
+//        //    // Act
+//        //    controller.Put(5, "value");
 
-        //    // Assert
-        //}
-    }
-}
+//        //    // Assert
+//        //}
+//    }
+//}
